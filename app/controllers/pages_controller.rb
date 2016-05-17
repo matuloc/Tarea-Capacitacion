6b49e24cb4b17336cbee65152fc13bcbc95924fb
matuloc <matulo.castro@gmail.com>
2016-05-17 16:44:49 -0400

    Tarea Capacitación

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,26 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def graficoa
+  end
+
+  def graficob
+  end
+
+  def graficoc
+  end
+
+  def graficod
+  end
+  
+  def graficoe
+  end
+
+  def tarea1
+  end
+
+  def tarea2
+  end
+
+end
---
+++ public/data.rb
@@ -0,0 +1,4 @@
+require 'json'
+
+file=File.read('data.json')
+data_hash=JSON.parse(file)
